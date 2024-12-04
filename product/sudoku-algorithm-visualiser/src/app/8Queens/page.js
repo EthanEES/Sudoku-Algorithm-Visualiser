d2d13b9a162141fde4080c55d5d9d13e7c0984e8
Ethan Eldridge-Smith <ZLAC231@live.rhul.ac.uk>
2024-12-04 17:20:56 +0000

    feat: Added a new page for the 8 queens problem

---
+++ product/sudoku-algorithm-visualiser/src/app/8Queens/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState } from "react";
+import SudokuGrid from "../../components/sudokugrid";
+
+export default function Page() {
+
+    return (
+        <div className="flex font-spacegrotesk">
+        </div>
+    );
+  }
